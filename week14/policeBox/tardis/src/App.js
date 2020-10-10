49be305be2eb47edd1872d22734b831653d3676b
Joey Bodoia <jbodoia21@cmc.edu>
2020-10-10 14:00:42 -0700

    change it function working on click

--- week14/policeBox/tardis/src/App.js
+++ week14/policeBox/tardis/src/App.js
@@ -3,9 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state,setState] = React.useState({
+    tardis:{
+      name: "Time and Relative Dimension in Space",
+      caps: false
+    }
+  })
+  const changeIt = (text) => {
+    if (state.tardis.caps) {
+      setState({
+        tardis: {
+          name: text.toLowerCase(),
+          caps: false
+        }
+      })
+    } else {
+      setState({
+        tardis: {
+          name: text.toUpperCase(),
+          caps: true
+        }
+      })
+    }
+
+  }
   return (
-    <div className="App">
-      <h1>Time and Relative Dimension in Space</h1>
+    <div className="App" onClick={()=>{changeIt(state.tardis.name)}}>
+      <h1 >{state.tardis.name}</h1>
     </div>
   );
 }
